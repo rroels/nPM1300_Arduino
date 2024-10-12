073c6609958d1819d385fac10a9630c917baa248
Reinout Roels <reinout.roels@gmail.com>
2024-10-12 14:45:07 +0200

    disable flash verification after upload

---
+++ scripts/no_verify.py
@@ -0,0 +1,10 @@
+Import("env")
+
+old_flags = env["UPLOADERFLAGS"]
+# filter out the --verify
+new_flags = [opt for opt in old_flags if opt != "--verify"]
+# replace and update
+env.Replace(
+    UPLOADERFLAGS=new_flags,
+    UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES"
+)
